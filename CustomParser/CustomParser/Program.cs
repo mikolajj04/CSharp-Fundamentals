a6adf3d966c534493a702e0f840941f47092668d
Mikolaj Jussak <Mikolajjussak@gmail.com>
2025-12-27 21:07:18 +0100

    Custom parser is done. -string must be parsable into intiger -Parsed number must be negative -Parser returns parsed value (if not parsable returns null) and boolean value (Parsed and Negative or not)

---
+++ CustomParser/CustomParser/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace CustomTryParse
+{
+
+
+    class Program
+    {
+
+        static public int? CustomTryParse(string valueToParse, out bool isParsableAndNegative)
+        {
+
+
+            if (int.TryParse(valueToParse, out int parsedValue) && parsedValue < 0)
+            {
+                isParsableAndNegative = true;
+                return parsedValue;
+
+            }
+            else
+            {
+                isParsableAndNegative = false;
+                return null;
+            }
+
+
+
+        }
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number to parse: ");
+                string value = Console.ReadLine();
+                bool isParsableAndNegative;
+                CustomTryParse(value, out isParsableAndNegative);
+                Console.WriteLine($"Value: {value}");
+                Console.WriteLine($"Is Parsable and nagative number?: {isParsableAndNegative}");
+                Console.WriteLine("-----------------------------------------------------\n");
+            }
+
+
+
+
+        }
+
+    }
+}
